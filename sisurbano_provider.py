5fcf48824019657599f4cd7e4c390f15ee2d9a26
Crash <fastuller88@gmail.com>
2019-09-30 17:18:04 -0500

    risk housing index added

--- sisurbano_provider.py
+++ sisurbano_provider.py
@@ -49,6 +49,7 @@ from .algs import (
     IB07SoilPermeability,
     IB08AgriculturalGrove,
     IC04Proximity2AlternativeTransport,
+    ID03HousingRisk,
     ID11Theft,
     ZN01FuzzyVectorial,
     ZN02AllocateValues,
@@ -90,6 +91,7 @@ class SisurbanoProvider(QgsProcessingProvider):
         self.addAlgorithm(IB07SoilPermeability.IB07SoilPermeability())
         self.addAlgorithm(IB08AgriculturalGrove.IB08AgriculturalGrove())
         self.addAlgorithm(IC04Proximity2AlternativeTransport.IC04Proximity2AlternativeTransport())
+        self.addAlgorithm(ID03HousingRisk.ID03HousingRisk())
         self.addAlgorithm(ID11Theft.ID11Theft())
         self.addAlgorithm(ZN01FuzzyVectorial.ZN01FuzzyVectorial())
         self.addAlgorithm(ZN02AllocateValues.ZN02AllocateValues())
